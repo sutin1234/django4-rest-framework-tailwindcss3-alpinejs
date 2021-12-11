5d5a47bc614cdd1bf40febe26f3845c26e728b51
Sutin <poly.dev2018@gmail.com>
2021-12-12 02:32:30 +0700

    add context to display

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index_home(request):
-    return render(request, "blog/index.html")
+    ctx = {
+        'text_hello': ' Hello Django4'
+    }
+    return render(request, "blog/index.html", ctx)
