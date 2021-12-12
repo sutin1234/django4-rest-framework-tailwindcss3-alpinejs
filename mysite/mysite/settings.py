09ef857db0c7aef0e2e1970a1c3715a7d883cafe
Sutin <poly.dev2018@gmail.com>
2021-12-13 01:02:44 +0700

    add config to deploy

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-nu6b6re8o293*+ia@c+)vu%-4fcwbjv9)s!k$b1zmqkqv4xvmb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'https://django4-rest-framework.herokuapp.com']
 
 # Application definition
 
